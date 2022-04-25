e78249a62e66eae7e7b381ee7f2ce0567b89b2bd
samuel orji <awesomeorji@gmail.com>
2022-04-25 19:59:27 +0100

    split bytes

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,53 @@ mod models;
 
 use std::env::Args;
 use std::fmt::format;
-use models::{SplitErrors, ByteUnit, SplitOptions, Config };
+use models::{SplitErrors, ByteUnit, SplitOptions, SplitOptions::*, Config };
 use clap::Parser;
 
-use std::io::{BufReader, BufRead, Error, ErrorKind, BufWriter, Write};
+use std::io::{BufReader, BufRead, Error, ErrorKind, BufWriter, Write, Read};
 use std::fs::{File, read};
 use std::num::ParseIntError;
 use std::process;
-use crate::SplitOptions::SplitByBytes;
 
 fn main() {
     let config = Config::parse();
+    let file_name = config.file_name.clone();
+    validate_and_parse(config).and_then(|splitOptions| {
+        call_appropriate_function(splitOptions)
+    }).unwrap_or_else(|e|print_error(e, file_name))
+}
 
-   // println!("{:?}",config);
-    let res = validate_config(config).expect("failure");
+fn print_error(error : SplitErrors, file_name: String) -> () {
+    match error {
+        SplitErrors::FILE_NOT_FOUND => {
+            println!("File {} not found",file_name)
+        }
+        SplitErrors::EMPTY_FILE => {
+            println!("File {} is empty",file_name)
+        }
 
-    println!("{:?}",res);
-    // run(&config).unwrap_or_else(|splitError| {
-    //     match splitError {
-    //         SplitErrors::FILE_NOT_FOUND => {
-    //             println!("File {} not found",&config.file_name)
-    //         }
-    //         SplitErrors::EMPTY_FILE => {
-    //             println!("File {} is empty",&config.file_name)
-    //         }
-    //         SplitErrors::InternalError(err) => {
-    //             eprintln!("Error splitting file:  {:?}",err)
-    //         }
-    //     }
-    // })
+        SplitErrors::InternalError(err) => {
+            eprintln!("Error splitting file:  {:?}",err)
+        }
+        SplitErrors::InvalidConfig(err) => {
+            println!("Invalid Config: {}",err)
+        }
+    }
 }
 
 
-fn validate_config(config : Config) -> Result<SplitOptions,SplitErrors> {
+fn call_appropriate_function(splitOptions : SplitOptions) -> Result<(),SplitErrors> {
+    match splitOptions {
+        p @ SplitOptions::SplitByLines { .. } => {
+            split_by_lines(p)
+        }
+        p @ SplitOptions::SplitByBytes { .. } => {
+            split_by_bytes(p)
+        }
+    }
+}
+
+fn validate_and_parse(config : Config) -> Result<SplitOptions,SplitErrors> {
     // cannot supply l and b args
     if let (Some(l), Some(b)) = (config.line_Length.as_ref(),config.byte_count.as_ref()) {
         return Err(SplitErrors::InvalidConfig(format!("Cannot supply both line length: {} and byte count : {} ",l,b)))
@@ -45,15 +59,12 @@ fn validate_config(config : Config) -> Result<SplitOptions,SplitErrors> {
         let byte_size_unit = char_vec[char_vec.len() - 1];
         let size = &char_vec[..char_vec.len() - 1];
 
-        match ByteUnit::parse(byte_size_unit) {
+        return match ByteUnit::parse(byte_size_unit) {
             Ok(byte_unit) => {
-
                 let byte_count_string = String::from_iter(size);
-
-
                 match byte_count_string.parse::<u64>() {
                     Ok(byte_length) => {
-                        return Ok(
+                        Ok(
                             SplitByBytes {
                                 byte_length,
                                 additional_suffix: config.additional_suffix,
@@ -63,16 +74,16 @@ fn validate_config(config : Config) -> Result<SplitOptions,SplitErrors> {
                         )
                     },
                     Err(e) => {
-                        return Err(SplitErrors::InvalidConfig(format!("Cannot parse {} as a number",byte_count_string)))
+                        Err(SplitErrors::InvalidConfig(format!("Cannot parse {} as a number", byte_count_string)))
                     }
                 }
             },
             Err(e) => {
-                return Err(SplitErrors::InvalidConfig(e))
+                Err(SplitErrors::InvalidConfig(e))
             }
         }
     } else {
-        Ok(SplitOptions::SplitByLines {
+        Ok(SplitByLines {
             line_length : config.line_Length.unwrap(),
             additional_suffix: config.additional_suffix,
             file_name : config.file_name
@@ -84,76 +95,168 @@ fn create_new_file(file_number: u32, additional_suffix: &str) -> std::io::Result
     File::create(format!("part{}{}", file_number, additional_suffix))
 }
 
-fn run(config : &Config) -> Result<(),SplitErrors> {
-    let mut is_empty = true;
+fn split_by_lines(conf: SplitOptions) -> Result<(),SplitErrors> {
 
-    let mut file_number: u32 = 0;
-    let mut cursor: u32 = 0;
+    match conf {
+        SplitOptions::SplitByLines{ line_length, additional_suffix, file_name} => {
+            let mut is_empty = true;
 
-    let file = File::open(&config.file_name)?;
+            let mut file_number: u32 = 0;
+            let mut cursor: u32 = 0;
 
-    // default buffer reader is 8KB
-    let mut reader = BufReader::new(file);
-    let mut writer = BufWriter::new(create_new_file(file_number, &config.additional_suffix)?);
-
-    fn write_to_writer(line: std::io::Result<String>, mut writer: &mut BufWriter<File>,cursor_num : &u32) -> std::io::Result<usize> {
-        let mut correct_line = line.map_err(|e| {
-            eprintln!("Cannot read line number {}", cursor_num);
-            e
-        })?;
-        correct_line.push('\n');
-        writer.write(correct_line.as_bytes())
-    }
-
-    for line in reader.lines(){
-        is_empty = false;
-
-        if cursor == config.line_Length.unwrap() {
-            // create new file
-            file_number += 1;
-            cursor = 0;
+            let file = File::open(&file_name)?;
 
 
-            // forcefully flush, so contents in buffer is written before changing underlying file
-            if let Err(e) =  writer.flush() {
-                return create_split_error("Cannot Flush buffer",e)
+            let mut reader = BufReader::new(file);
+            let mut writer = BufWriter::new(create_new_file(file_number, &additional_suffix)?);
+
+            fn write_to_writer(line: std::io::Result<String>, mut writer: &mut BufWriter<File>,cursor : &mut u32) -> std::io::Result<usize> {
+                let mut correct_line = line.map_err(|e| {
+                    eprintln!("Cannot read line number {}", cursor);
+                    e
+                })?;
+                correct_line.push('\n');
+                *cursor +=1;
+                writer.write(correct_line.as_bytes())
             }
-            let mut underlying_file = writer.get_mut();
 
-            if let Err(e) = create_new_file(file_number, &config.additional_suffix) {
-                return create_split_error("Cannot create new file",e)
-            }
-            match create_new_file(file_number, &config.additional_suffix) {
-                Ok(new_file) => {
-                    *underlying_file = new_file
+            for line in reader.lines(){
+                is_empty = false;
 
+                if cursor == line_length {
+                    // create new file
+                    file_number += 1;
+                    cursor = 0;
+
+
+                    // forcefully flush, so contents in buffer is written before changing underlying file
+                    if let Err(e) =  writer.flush() {
+                        return create_split_error("Cannot Flush buffer",e)
+                    }
+                    let mut underlying_file = writer.get_mut();
+
+                    if let Err(e) = create_new_file(file_number, &additional_suffix) {
+                        return create_split_error("Cannot create new file",e)
+                    }
+                    match create_new_file(file_number, &additional_suffix) {
+                        Ok(new_file) => {
+                            *underlying_file = new_file
+
+                        }
+                        Err(e) => {
+                            return create_split_error("Cannot create new file",e)
+                        }
+                    }
+
+                    if let Err(e) = { write_to_writer(line, &mut writer,&mut cursor) } {
+                        return create_split_error("Cannot Write to buffer",e)
+                    }
+                } else {
+                    if let Err(e) = { write_to_writer(line, &mut writer,&mut cursor) } {
+                        return create_split_error("Cannot Write to buffer",e)
+                    }
                 }
-                Err(e) => {
-                    return create_split_error("Cannot create new file",e)
-                }
             }
 
-            if let Err(e) = {
-                cursor += 1;
-                write_to_writer(line, &mut writer,&cursor)
-            } {
-                return create_split_error("Cannot Write to buffer",e)
-            }
-        } else {
-            if let Err(e) = {
-                cursor += 1;
-                write_to_writer(line, &mut writer,&cursor)
-            } {
-                return create_split_error("Cannot Write to buffer",e)
+            if is_empty {
+                return Err(SplitErrors::EMPTY_FILE)
+            } else {
+                Ok(())
             }
         }
+        _ => {
+            Err(SplitErrors::InternalError(format!("Calling {:?} on a split by lines method",conf)))
+        }
+    }
+}
+
+fn split_by_bytes(conf: SplitOptions) -> Result<(),SplitErrors> {
+    match conf {
+        SplitOptions::SplitByBytes { byte_length, additional_suffix,file_name, byte_unit } => {
+
+            let mut is_empty = true;
+            let mut file_number: u32 = 0;
+            let mut cursor: u32 = 0;
+            let mut count = 0;
+            let mut reached_EOF = false;
+
+            if let Some(num_iterations) = match byte_unit {
+                ByteUnit::KB => Some(byte_length),
+                ByteUnit::MB => byte_length.checked_mul(1000)
+            } {
+
+                let file = File::open(&file_name)?;
+
+
+                let mut reader = BufReader::new(file);
+                let mut writer = BufWriter::new(create_new_file(file_number, &additional_suffix)?);
+
+                // 1KB Buffer
+                let mut buffer = [0_u8; 1024];
+                loop {
+                    count +=1;
+                    let mut nread = 0usize;
+                    while nread < buffer.len() {
+                        match reader.read(&mut buffer[nread..]) {
+                            Ok(0) =>  {
+                               if is_empty {
+                                   return Err(SplitErrors::EMPTY_FILE);
+                               } else {
+                                   if reached_EOF {
+                                       return Ok(())
+                                   } else {
+                                       reached_EOF = true;
+                                       break;
+                                   }
+                               }
+                            },
+                            Ok(n) => {
+                                is_empty = false;
+                               nread += n ;
+                            },
+                            Err(e) => {
+                                return Err(SplitErrors::InternalError(format!("Cannot read file : {:?}", e)));
+                            }
+                        }
+                    }
+
+                    if  count > num_iterations {
+                        file_number += 1;
+                        count = 1;
+                        // forcefully flush, so contents in buffer is written before changing underlying file
+                        if let Err(e) =  writer.flush() {
+                            return create_split_error("Cannot Flush buffer",e)
+                        }
+                        let mut underlying_file = writer.get_mut();
+
+                        if let Err(e) = create_new_file(file_number, &additional_suffix) {
+                            return create_split_error("Cannot create new file",e)
+                        }
+                        match create_new_file(file_number, &additional_suffix) {
+                            Ok(new_file) => {
+                                *underlying_file = new_file
+
+                            }
+                            Err(e) => {
+                                return create_split_error("Cannot create new file",e)
+                            }
+                        }
+                        writer.write_all(&buffer[..nread])?;
+                    } else {
+                        writer.write_all(&buffer[..nread])?;
+                    }
+                }
+            } else {
+                return Err(SplitErrors::InvalidConfig("Byte count too large".to_string()))
+            }
+        },
+       _ => {
+            return Err(SplitErrors::InternalError(format!("Calling {:?} on a split by bytes method",conf)))
+        }
+
     }
 
-    if is_empty {
-        return Err(SplitErrors::EMPTY_FILE)
-    } else {
-        Ok(())
-    }
+    Ok(())
 }
 
 
